3acbe5da4786188cc7d2b0f1f107a37b314b67d3
unknown <leandro.florentin@hotmail.com>
2024-02-29 21:38:05 -0300

    HABILIDADES

--- src/pages/habilidades.jsx
+++ src/pages/habilidades.jsx
@@ -15,33 +15,37 @@ const Habilidades = ({ reference }) => {
   return (
     <section ref={reference} id="habilidades" className="h-auto pt-5">
       <div className="tab-content no-select-text">
-        <div className="border-start border-bottom p-4">
+        <div className="p-2">
           <h5>Front-End</h5>
-          <div className="d-flex flex-wrap gap-2">
+          <div className="d-flex flex-wrap gap-2 mt-3">
             {badgesFront.map((value, key) => (<div key={key}><Badge className="no-select-text badge rounded-pill bg-custom padding-badge scale pointer" description={value} /></div>))}
           </div>
         </div>
-        <div className="border-end border-bottom p-4">
+        <hr />
+        <div className="p-2">
           <h5>Back-End</h5>
-          <div className="d-flex flex-wrap gap-2">
+          <div className="d-flex flex-wrap gap-2 mt-3">
             {badgesBack.map((value, key) => (<div key={key}><Badge className="no-select-text badge rounded-pill bg-custom padding-badge scale pointer" description={value} /></div>))}
           </div>
         </div>
-        <div className="p-4 border-start border-bottom">
+        <hr />
+        <div className="p-2">
           <h5>AWS</h5>
-          <div className="d-flex flex-wrap gap-2 ">
+          <div className="d-flex flex-wrap gap-2 mt-3 ">
             {badgesAws.map((value, key) => (<div key={key}><Badge className="no-select-text badge rounded-pill bg-custom padding-badge scale pointer" description={value} /></div>))}
           </div>
         </div>
-        <div className="p-4 border-end border-bottom" >
+        <hr />
+        <div className="p-2" >
           <h5>Software</h5>
-          <div className="d-flex flex-wrap gap-2  ">
+          <div className="d-flex flex-wrap gap-2 mt-3">
             {badgesSoftwate.map((value, key) => (<div key={key}><Badge className="no-select-text badge rounded-pill bg-custom padding-badge scale pointer" description={value} /></div>))}
           </div>
         </div>
-        <div className="p-4 border-start border-bottom">
+        <hr />
+        <div className="p-2">
           <h5>Metodologías</h5>
-          <div className="d-flex flex-wrap gap-2">
+          <div className="d-flex flex-wrap gap-2 mt-3">
             {badgesMetodologias.map((value, key) => (<div key={key}><Badge className="no-select-text badge rounded-pill bg-custom padding-badge scale pointer" description={value} /></div>))}
           </div>
         </div>
